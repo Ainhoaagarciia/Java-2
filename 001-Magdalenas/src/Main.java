353c7a4b0bb02aa54dabcced719bf5750418d528
AinhoaGarcíaSanz <ainhoagarciasanz02@gmail.com>
2025-01-09 19:07:36 +0100

    Clases Y Objetos

---
+++ 001-Magdalenas/src/Main.java
@@ -0,0 +1,23 @@
+import pojo.Magdalena;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //MAGDALENA
+        Magdalena magdalena= new Magdalena("Marrón","Chocolate","Circular","Nata");
+            magdalena.hornear();
+            magdalena.partirla();
+
+            /*magdalena.mezclar();
+            magdalena.vender();
+            magdalena.decorar();
+            magdalena.establecerSabor();
+            magdalena.mojar();
+            magdalena.anyadirPepitas();
+            magdalena.comer();
+            magdalena.envolver();
+            magdalena.coger();*/
+
+    }
+}
