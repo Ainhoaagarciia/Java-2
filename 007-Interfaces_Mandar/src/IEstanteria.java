547650110e21ed5c4d2dea716d224df8e53aad1f
AinhoaGarcíaSanz <ainhoagarciasanz02@gmail.com>
2025-02-04 19:45:06 +0100

    Delete 007-Interfaces_Mandar directory

--- 007-Interfaces_Mandar/src/IEstanteria.java
+++
@@ -1,7 +0,0 @@
-
-public interface IEstanteria  {
-    public void addCancion(Cancion c);
-    public void updateCancion(Cancion c);
-    public void deleteCancion(Cancion c);
-    public void imprimir();
-}
